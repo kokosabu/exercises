1cc31b2ec5838df29d17ba14524d24ab8ad2fba7
MIURA Yasuyuki <kokosabu@gmail.com>
2017-01-11 23:45:18 +0900

    4.4. Comments

---
+++ The_Rust_Programming_Language/4_4_comments/src/main.rs
@@ -0,0 +1,14 @@
+//! # The Rust Standard Library
+//!
+//! The Rust Standard Library provides the essential runtime
+//! functionality for building portable Rust software.
+
+fn main() {
+    // println!("01 Hello, world!");
+    /// println!("02 Hello, world!");
+    // println!("03 Hello, world!");
+    /* println!("04 Hello, world!");
+    println!("05 Hello, world!"); */
+    println!("06 Hello, world!");
+    println!("07 Hello, world!");
+}
