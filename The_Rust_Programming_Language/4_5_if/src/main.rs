6862d4dd7c99ab89116cb2520b85ccc68ed38441
MIURA Yasuyuki <kokosabu@gmail.com>
2017-01-13 23:35:20 +0900

    4.5. if

--- The_Rust_Programming_Language/4_5_if/src/main.rs
+++ The_Rust_Programming_Language/4_5_if/src/main.rs
@@ -3,5 +3,19 @@ fn main() {
 
     if x == 5 {
         println!("x is five!");
+    } else if x == 6 {
+        println!("x is six!");
+    } else {
+        println!("x is not five or six :(");
     }
+
+    let y = if x == 5 {
+        10
+    } else {
+        15
+    }; // y: i32
+
+    println!("y is {}", y);
+
+    println!("? is {}", if x == 5 { 10 } else { 15 });
 }
