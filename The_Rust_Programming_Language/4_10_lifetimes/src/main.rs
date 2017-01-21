e36042af33dace1f19a39f58f7499b3f61a1769b
MIURA Yasuyuki <kokosabu@gmail.com>
2017-01-21 23:08:40 +0900

    4.10. Lifetimes

--- The_Rust_Programming_Language/4_10_lifetimes/src/main.rs
+++ The_Rust_Programming_Language/4_10_lifetimes/src/main.rs
@@ -1,9 +1,36 @@
+struct Foo<'a> {
+    x: &'a i32,
+}
+
 fn main() {
+    /*
     let r;              // Introduce reference: r
     {
         let i = 1;      // Introduce scoped value: i
         r = &i;         // Store reference of i in r
     }                   // i goes out of scope and is dropped.
-
     println!("{}", r);  // r still refers to i
+    */
+
+    /*
+    //fn skip_prefix(line: &str, prefix: &str) -> &str {
+    //fn skip_prefix<'a, 'b>(line: &'a str, prefix: &'b str) -> &'a str {
+        // ...
+    //}
+
+    let line = "lang:en=Hello World!";
+    let lang = "en";
+
+    let v;
+    {
+        let p = format!("lang:{}=", lang);  // -+ p goes into scope
+        v = skip_prefix(line, p.as_str());  //  |
+    }                                       // -+ p goes out of scope
+    println!("{}", v);
+    */
+
+    let y = &5; // this is the same as `let _y = 5; let y = &_y;`
+    let f = Foo { x: y };
+
+    println!("{}", f.x);
 }
