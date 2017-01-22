439e1a0efe155c9a48f944431e9d42bedd85c534
MIURA Yasuyuki <kokosabu@gmail.com>
2017-01-22 15:22:52 +0900

    4.10. Lifetimes

--- The_Rust_Programming_Language/4_10_lifetimes/src/main.rs
+++ The_Rust_Programming_Language/4_10_lifetimes/src/main.rs
@@ -2,6 +2,10 @@ struct Foo<'a> {
     x: &'a i32,
 }
 
+impl<'a> Foo<'a> {
+    fn x(&self) -> &'a i32 { self.x }
+}
+
 fn main() {
     /*
     let r;              // Introduce reference: r
@@ -32,5 +36,5 @@ fn main() {
     let y = &5; // this is the same as `let _y = 5; let y = &_y;`
     let f = Foo { x: y };
 
-    println!("{}", f.x);
+    println!("{}", f.x());
 }
