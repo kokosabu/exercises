869c44cbb747abd37203911f6a395e38fe42aab3
MIURA Yasuyuki <kokosabu@gmail.com>
2017-02-03 08:31:28 +0900

    4.19. Traits

--- The_Rust_Programming_Language/4_19_traits/src/main.rs
+++ The_Rust_Programming_Language/4_19_traits/src/main.rs
@@ -71,6 +71,50 @@ impl HasArea for i32 {
     }
 }
 
+use std::fmt::Debug;
+
+fn foo<T: Clone, K: Clone + Debug>(x: T, y: K) {
+    x.clone();
+    y.clone();
+    println!("{:?}", y);
+}
+
+fn bar<T, K>(x: T, y: K) where T: Clone, K: Clone + Debug {
+    x.clone();
+    y.clone();
+    println!("{:?}", y);
+}
+
+trait Foo {
+    fn is_valid(&self) -> bool;
+
+    fn is_invalid(&self) -> bool { !self.is_valid() }
+}
+
+struct UseDefault;
+
+impl Foo for UseDefault {
+    fn is_valid(&self) -> bool {
+        println!("Called UseDefault.is_valid.");
+        true
+    }
+}
+
+struct OverrideDefault;
+
+impl Foo for OverrideDefault {
+    fn is_valid(&self) -> bool {
+        println!("Called OverrideDefault.is_valid.");
+        true
+    }
+
+    fn is_invalid(&self) ->
+        bool {
+            println!("Called OverrideDefault.is_invalid!");
+            true // overrides the expected value of is_invalid()
+        }
+}
+
 fn main() {
     let c = Circle {
         x: 0.0f64,
@@ -100,4 +144,13 @@ fn main() {
     assert!(!r.is_square());
 
     5.area();
+
+    foo("Hello", "world");
+    bar("Hello", "world");
+
+    let default = UseDefault;
+    assert!(!default.is_invalid()); // prints "Called UseDefault.is_valid."
+
+    let over = OverrideDefault;
+    assert!(over.is_invalid()); // prints "Called OverrideDefault.is_invalid!"
 }
