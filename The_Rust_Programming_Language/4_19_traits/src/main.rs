20bbc010939338f6f401f9c75505e7db4544b14c
MIURA Yasuyuki <kokosabu@gmail.com>
2017-02-04 06:35:08 +0900

    4.19. Traits

--- The_Rust_Programming_Language/4_19_traits/src/main.rs
+++ The_Rust_Programming_Language/4_19_traits/src/main.rs
@@ -115,6 +115,26 @@ impl Foo for OverrideDefault {
         }
 }
 
+/*
+trait Foo {
+    fn foo(&self);
+}
+
+trait FooBar : Foo {
+    fn foobar(&self);
+}
+
+struct Baz;
+
+impl Foo for Baz {
+    fn foo(&self) { println!("foo"); }
+}
+
+impl FooBar for Baz {
+    fn foobar(&self) { println!("foobar"); }
+}
+*/
+
 fn main() {
     let c = Circle {
         x: 0.0f64,
