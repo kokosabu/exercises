7c2db69fe50e0dfc407b8f900c6083b9586b69a4
MIURA Yasuyuki <kokosabu@gmail.com>
2017-02-12 10:29:40 +0900

    4.25. Crates and Modules

--- The_Rust_Programming_Language/phrases/src/lib.rs
+++ The_Rust_Programming_Language/phrases/src/lib.rs
@@ -1,24 +1,2 @@
-/*
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-    }
-}
-*/
-
-mod english {
-    mod greetings {
-    }
-
-    mod farewells {
-    }
-}
-
-mod japanese {
-    mod greetings {
-    }
-
-    mod farewells {
-    }
-}
+pub mod english;
+mod japanese;
