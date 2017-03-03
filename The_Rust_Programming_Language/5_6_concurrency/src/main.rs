8b5b32cdd054d4ebc5b686055ee15a290b6728e6
MIURA Yasuyuki <kokosabu@gmail.com>
2017-03-03 20:30:00 +0900

    5.6. Concurrency

--- The_Rust_Programming_Language/5_6_concurrency/src/main.rs
+++ The_Rust_Programming_Language/5_6_concurrency/src/main.rs
@@ -1,20 +1,26 @@
-use std::sync::{Arc, Mutex};
 use std::thread;
-use std::time::Duration;
+use std::sync::mpsc;
 
 fn main() {
-	let data = Arc::new(Mutex::new(vec![1, 2, 3]));
+    let (tx, rx) = mpsc::channel();
 
-    for i in 0..3 {
-        let data = data.clone();
+    for i in 0..10 {
+        let tx = tx.clone();
 
         thread::spawn(move || {
-			let mut data = data.lock().unwrap();
-            data[0] += i;
+            let answer = i * i;
+
+            tx.send(answer).unwrap();
         });
     }
 
-	thread::sleep(Duration::from_millis(50));
+    for _ in 0..10 {
+        println!("{}", rx.recv().unwrap());
+    }
 
-	println!("{:?}", data);
+	let handle = thread::spawn(move || {
+		panic!("oops!");
+	});
+	let result = handle.join();
+	assert!(result.is_err());
 }
