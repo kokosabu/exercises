e1aae98b213a7a625e86871c275f74763656e3dc
MIURA Yasuyuki <kokosabu@gmail.com>
2017-03-05 16:04:43 +0900

    5.7.Error Handling

--- The_Rust_Programming_Language/5_7_error_handling/src/main.rs
+++ The_Rust_Programming_Language/5_7_error_handling/src/main.rs
@@ -9,10 +9,57 @@ fn find(haystack: &str, needle: char) -> Option<usize> {
     None
 }
 
-fn main() {
-    let file_name = "foobar.rs";
+// Returns the extension of the given file name, where the extension is defined
+// as all characters following the first `.`.
+// If `file_name` has no `.`, then `None` is returned.
+fn extension_explicit(file_name: &str) -> Option<&str> {
     match find(file_name, '.') {
-        None => println!("No file extension found."),
-        Some(i) => println!("File extension: {}", &file_name[i+1..]),
+        None => None,
+        Some(i) => Some(&file_name[i+1..]),
     }
 }
+
+fn map<F, T, A>(option: Option<T>, f: F) -> Option<A> where F: FnOnce(T) -> A {
+    match option {
+        None => None,
+        Some(value) => Some(f(value)),
+    }
+}
+
+// Returns the extension of the given file name, where the extension is defined
+// as all characters following the first `.`.
+// If `file_name` has no `.`, then `None` is returned.
+fn extension(file_name: &str) -> Option<&str> {
+    find(file_name, '.').map(|i| &file_name[i+1..])
+}
+
+fn unwrap_or<T>(option: Option<T>, default: T) -> T {
+    match option {
+        None => default,
+        Some(value) => value,
+    }
+}
+
+fn main() {
+    assert_eq!(extension("foobar.csv").unwrap_or("rs"), "csv");
+    assert_eq!(extension("foobar").unwrap_or("rs"), "rs");
+}
+
+fn file_path_ext_explicit(file_path: &str) -> Option<&str> {
+    match file_name(file_path) {
+        None => None,
+        Some(name) => match extension(name) {
+            None => None,
+            Some(ext) => Some(ext),
+        }
+    }
+}
+
+fn file_name(file_path: &str) -> Option<&str> {
+  // Implementation elided.
+  unimplemented!()
+}
+
+// fn file_path_ext(file_path: &str) -> Option<&str> {
+//     file_name(file_path).map(|x| extension(x)) // This causes a compilation error.
+// }
