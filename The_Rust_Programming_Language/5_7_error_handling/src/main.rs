15148894c0babad8d5b0de33d9a8e9fa74726216
MIURA Yasuyuki <kokosabu@gmail.com>
2017-03-08 23:42:05 +0900

    5.7.Error Handling

--- The_Rust_Programming_Language/5_7_error_handling/src/main.rs
+++ The_Rust_Programming_Language/5_7_error_handling/src/main.rs
@@ -2,15 +2,25 @@ use std::fs::File;
 use std::io::Read;
 use std::path::Path;
 
-fn file_double<P: AsRef<Path>>(file_path: P) -> i32 {
-    let mut file = File::open(file_path).unwrap(); // error 1
-    let mut contents = String::new();
-    file.read_to_string(&mut contents).unwrap(); // error 2
-    let n: i32 = contents.trim().parse().unwrap(); // error 3
-    2 * n
+fn file_double<P: AsRef<Path>>(file_path: P) -> Result<i32, String> {
+    File::open(file_path)
+         .map_err(|err| err.to_string())
+         .and_then(|mut file| {
+              let mut contents = String::new();
+              file.read_to_string(&mut contents)
+                  .map_err(|err| err.to_string())
+                  .map(|_| contents)
+         })
+         .and_then(|contents| {
+              contents.trim().parse::<i32>()
+                      .map_err(|err| err.to_string())
+         })
+         .map(|n| 2 * n)
 }
 
 fn main() {
-    let doubled = file_double("foobar");
-    println!("{}", doubled);
+    match file_double("foobar") {
+        Ok(n) => println!("{}", n),
+        Err(err) => println!("Error: {}", err),
+    }
 }
