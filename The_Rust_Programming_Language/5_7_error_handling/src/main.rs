51d6a012abb980d7fd5403c6e1764b200c45d05f
MIURA Yasuyuki <kokosabu@gmail.com>
2017-03-09 23:54:04 +0900

    5.7.Error Handling

--- The_Rust_Programming_Language/5_7_error_handling/src/main.rs
+++ The_Rust_Programming_Language/5_7_error_handling/src/main.rs
@@ -1,20 +1,19 @@
+macro_rules! try {
+    ($e:expr) => (match $e {
+        Ok(val) => val,
+        Err(err) => return Err(err),
+    });
+}
+
 use std::fs::File;
 use std::io::Read;
 use std::path::Path;
 
 fn file_double<P: AsRef<Path>>(file_path: P) -> Result<i32, String> {
-    let mut file = match File::open(file_path) {
-        Ok(file) => file,
-        Err(err) => return Err(err.to_string()),
-    };
+    let mut file = try!(File::open(file_path).map_err(|e| e.to_string()));
     let mut contents = String::new();
-    if let Err(err) = file.read_to_string(&mut contents) {
-        return Err(err.to_string());
-    }
-    let n: i32 = match contents.trim().parse() {
-        Ok(n) => n,
-        Err(err) => return Err(err.to_string()),
-    };
+    try!(file.read_to_string(&mut contents).map_err(|e| e.to_string()));
+    let n = try!(contents.trim().parse::<i32>().map_err(|e| e.to_string()));
     Ok(2 * n)
 }
 
@@ -24,5 +23,3 @@ fn main() {
         Err(err) => println!("Error: {}", err),
     }
 }
-
-
