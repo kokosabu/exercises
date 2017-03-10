8619d8b1404af610c65374c03ea198236146c2d9
MIURA Yasuyuki <kokosabu@gmail.com>
2017-03-10 23:52:21 +0900

    5.7.Error Handling

--- The_Rust_Programming_Language/5_7_error_handling/src/main.rs
+++ The_Rust_Programming_Language/5_7_error_handling/src/main.rs
@@ -1,25 +1,56 @@
-macro_rules! try {
-    ($e:expr) => (match $e {
-        Ok(val) => val,
-        Err(err) => return Err(err),
-    });
+use std::fmt::{Debug, Display};
+
+trait Error: Debug + Display {
+  /// A short description of the error.
+  fn description(&self) -> &str;
+
+  /// The lower level cause of this error, if any.
+  fn cause(&self) -> Option<&Error> { None }
 }
 
-use std::fs::File;
-use std::io::Read;
-use std::path::Path;
+use std::io;
+use std::num;
 
-fn file_double<P: AsRef<Path>>(file_path: P) -> Result<i32, String> {
-    let mut file = try!(File::open(file_path).map_err(|e| e.to_string()));
-    let mut contents = String::new();
-    try!(file.read_to_string(&mut contents).map_err(|e| e.to_string()));
-    let n = try!(contents.trim().parse::<i32>().map_err(|e| e.to_string()));
-    Ok(2 * n)
+// We derive `Debug` because all types should probably derive `Debug`.
+// This gives us a reasonable human readable description of `CliError` values.
+#[derive(Debug)]
+enum CliError {
+    Io(io::Error),
+    Parse(num::ParseIntError),
 }
 
-fn main() {
-    match file_double("foobar") {
-        Ok(n) => println!("{}", n),
-        Err(err) => println!("Error: {}", err),
+use std::error;
+use std::fmt;
+
+impl fmt::Display for CliError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            // Both underlying errors already impl `Display`, so we defer to
+            // their implementations.
+            CliError::Io(ref err) => write!(f, "IO error: {}", err),
+            CliError::Parse(ref err) => write!(f, "Parse error: {}", err),
+        }
+    }
+}
+
+impl error::Error for CliError {
+    fn description(&self) -> &str {
+        // Both underlying errors already impl `Error`, so we defer to their
+        // implementations.
+        match *self {
+            CliError::Io(ref err) => err.description(),
+            CliError::Parse(ref err) => err.description(),
+        }
+    }
+
+    fn cause(&self) -> Option<&error::Error> {
+        match *self {
+            // N.B. Both of these implicitly cast `err` from their concrete
+            // types (either `&io::Error` or `&num::ParseIntError`)
+            // to a trait object `&Error`. This works because both error types
+            // implement `Error`.
+            CliError::Io(ref err) => Some(err),
+            CliError::Parse(ref err) => Some(err),
+        }
     }
 }
